dfde2d7b99cd4865780de65a0100586576091848
Samuel Serra <sserradev@gmail.com>
2022-10-20 07:49:52 -0300

    tirando o modo dark padrao

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <ColorModeScript initialColorMode="dark"></ColorModeScript>
+      <ColorModeScript></ColorModeScript>
       <App />
     </ChakraProvider>
   </React.StrictMode>
