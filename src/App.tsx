ac4875daf0667ca4c0b3f12d0d6ee13cc70be702
Samuel Serra <sserradev@gmail.com>
2022-10-20 07:41:55 -0300

    label

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Flex, VStack } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
+import "./global.css";
 
 function App() {
   return (
